0e4d9ce58aab2190359b9f858e8cb95de715bec3
khimoo <dailysentence1111@gmail.com>
2026-02-05 11:38:57 +0900

    nix runに対応させた

--- flake.nix
+++ flake.nix
@@ -9,22 +9,44 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { # system パラメータを渡す
+        pkgs = import nixpkgs {
           inherit system;
+        };
+
+        pythonEnv = pkgs.python3.withPackages (python-pkgs: [
+          python-pkgs.matplotlib
+          python-pkgs.numpy
+          python-pkgs.networkx
+          python-pkgs.scipy
+          python-pkgs.cvxpy
+          python-pkgs.ecos
+        ]);
+
+        app = pkgs.stdenv.mkDerivation {
+          name = "provably-good-planar-mappings";
+          src = ./.;
+          buildInputs = [ pythonEnv ];
+          installPhase = ''
+            mkdir -p $out/bin $out/share
+            cp *.py $out/share/
+            
+            echo "#!${pkgs.bash}/bin/bash" > $out/bin/run-app
+            echo "exec ${pythonEnv}/bin/python $out/share/ui-for-refactored2.py" >> $out/bin/run-app
+            chmod +x $out/bin/run-app
+          '';
         };
       in
       {
+        packages.default = app;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = app;
+          exePath = "/bin/run-app";
+        };
+
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-          (pkgs.python3.withPackages (python-pkgs: [
-            python-pkgs.matplotlib
-            python-pkgs.numpy
-            python-pkgs.networkx
-            python-pkgs.scipy
-            python-pkgs.cvxpy
-            python-pkgs.ecos
-          ]))
+          packages = [
+            pythonEnv
           ];
         };
       }
